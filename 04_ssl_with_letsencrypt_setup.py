f505bb3d878e0ccb2acecea1e1867fa3d42be6cc
crepic21 <crepic21@gmail.com>
2022-10-29 21:49:20 +0200

    added ssl setup

---
+++ 04_ssl_with_letsencrypt_setup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""subprocess library allows us to execute and manage subprocesses directly from Python"""
+import subprocess
+
+subprocess.call(["sudo", "apt-get", "install", "python3-certbot-nginx"])
+subprocess.call(["sudo", "certbot", "--nginx", "-d", "wordpresslinux.xyz", "-d", "www.wordpresslinux.xyz"])
